8a3c584aa36a9e602c077ca4634113745d50ccfc
Rishabh <jharishabh2k3@gmail.com>
2023-06-25 18:35:43 +0530

    [FIX] Use xfce4-terminal to execute scripts

--- welcome.sh
+++ welcome.sh
@@ -2,10 +2,10 @@
 
 yad --title "Welcome To Tcet-Linux" --form --columns=3 --width=740 --height=540 --text="This is test tcet-linux Welcome app trail" --image=$HOME/Desktop/welcome/tcetlinux-logo.png  --center \
     --scale 2.0\
-    --field="<b>Update</b>":fbtn "gnome-terminal -- sh ./scripts/update.sh" \
+    --field="<b>Update</b>":fbtn "xfce4-terminal -x ./scripts/update.sh" \
     --field="<b>Github</b>":fbtn "sh ./scripts/github.sh" \
     --field="<b>Discord</b>":fbtn "sh ./scripts/discord.sh" \
-    --field="<b>Update Mirror</b>":fbtn "gnome-terminal -- sh ./scripts/mirror.sh" \
-    --field="<b>Github</b>":fbtn "konsole --noclose -e sh './scripts/drivers'" \
-    --field="<b>Discord</b>":fbtn "konsole --noclose -e sh './scripts/flatpakz'" \
+    --field="<b>Update Mirror</b>":fbtn "xfce4-terminal -x ./scripts/mirror.sh" \
+    --field="<b>TBD</b>":fbtn "echo "TO BE DECIDED"" \
+    --field="<b>Screen Resolution</b>":fbtn "xfce4-terminal -x  './scripts/screen-resolution.sh'" \
     --button=Exit:1
