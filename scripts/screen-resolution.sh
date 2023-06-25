5748a9bc6f94fcb6f2f8f839ccd7872de9cf76a2
0xAtharv <atharva.a.vartak@gmail.com>
2023-06-25 18:22:34 +0530

    [ADD] Added screen resolution fix script

---
+++ scripts/screen-resolution.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash 
+
+# get no of displays connected
+
+NDISPLAYS=$(xrandr | grep " connected" |wc -l) 
+
+for i in $(seq 1 $NDISPLAYS)
+do
+    # Get list of displays 
+    tempDispList=$(xrandr | grep " connected" | cut -d " " -f 1 )
+
+        
+    tempDisp=$(echo $tempDispList| cut -d " " -f "$i")
+
+    echo $tempDisp
+
+    # Change resolution of individual displays
+    xrandr --output $tempDisp --mode 1920x1080
+    
+done
