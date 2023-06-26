dfd8c939c8b01120cc30a261aef565058e38f70d
0xAtharv <atharva.a.vartak@gmail.com>
2023-06-26 19:09:59 +0530

    [FIX] Fix welcome app entries

--- welcome.sh
+++ welcome.sh
@@ -2,15 +2,14 @@
 
 yad --title "Welcome To Tcet-Linux" \
     --form \
-    --columns=2 \
+    --columns=2\
     --width=640 \
-    --height=300\
-    --text="This is test tcet-linux Welcome app trail"\
+    --height=380 \
+    --text="This is test tcet-linux Welcome app trial" \
     --image=./assets/tcetlinux-logo.png \
-    --center \
-    --field="<b>Update</b>":fbtn "bash -c './scripts/update.sh'" \
-    --field="<b>Github</b>":fbtn "bash -c  './scripts/github.sh'" \
-    --field="<b>Discord</b>":fbtn "bash -c './scripts/discord.sh'" \
+    --field="<b>Update the PC</b>":fbtn "bash -c './scripts/update.sh'" \
+    --field="<b>Our Github Profile</b>":fbtn "bash -c  './scripts/github.sh'" \
+    --field="<b>Our Discord</b>":fbtn "bash -c './scripts/discord.sh'" \
     --field="<b>Update Mirror</b>":fbtn "xfce4-terminal -x './scripts/mirror.sh'" \
     --field="<b>Arch Wiki</b>":fbtn "bash -c './scripts/archwiki.sh'" \
     --field="<b>Screen Resolution</b>":fbtn "xfce4-terminal -x  './scripts/screen-resolution.sh'" \
