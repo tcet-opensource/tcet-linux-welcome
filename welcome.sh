68cb4f2a23b1d11bc0cca2ace49234c12a556d8e
0xAtharv <atharva.a.vartak@gmail.com>
2023-06-26 11:34:59 +0530

    [FIX] fixes in welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -1,11 +1,10 @@
 #!/bin/env bash
 
-yad --title "Welcome To Tcet-Linux" --form --columns=2 --width=640 --height=300 --text="This is test tcet-linux Welcome app trail" --image=./tcetlinux-logo.png  --center \
-    --scale 2.0\
-    --field="<b>Update</b>":fbtn "xfce4-terminal -x ./scripts/update.sh" \
-    --field="<b>Github</b>":fbtn "sh ./scripts/github.sh" \
-    --field="<b>Discord</b>":fbtn "sh ./scripts/discord.sh" \
-    --field="<b>Update Mirror</b>":fbtn "xfce4-terminal -x ./scripts/mirror.sh" \
-    --field="<b>TBD</b>":fbtn "echo "TO BE DECIDED"" \
+yad --title "Welcome To Tcet-Linux" --form --columns=2 --width=640 --height=300 --text="This is test tcet-linux Welcome app trail" --image=./assets/tcetlinux-logo.png --center \
+    --field="<b>Update</b>":fbtn "bash -c './scripts/update.sh'" \
+    --field="<b>Github</b>":fbtn "bash -c  './scripts/github.sh'" \
+    --field="<b>Discord</b>":fbtn "bash -c './scripts/discord.sh'" \
+    --field="<b>Update Mirror</b>":fbtn "xfce4-terminal -x './scripts/mirror.sh'" \
+    --field="<b>TBD</b>":fbtn "echo 'TO BE DECIDED'" \
     --field="<b>Screen Resolution</b>":fbtn "xfce4-terminal -x  './scripts/screen-resolution.sh'" \
     --button=Exit:1
