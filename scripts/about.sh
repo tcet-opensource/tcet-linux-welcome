395b90781e1aa5e30247a03ead8346a3b7e1f541
harshau007 <amanupadhyay2004@gmail.com>
2023-06-27 12:57:40 +0530

    [ADDED] about.sh and aur.sh

---
+++ scripts/about.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+yad --title="About Us" \
+    --window-icon=./assets/tcetlinux-logo.png \
+    --image=./assets/tcetlinux-logo.png \
+    --width=600 \
+    --height=250 \
+    --justify=center \
+    --text="<b>TCET Linux is GNU/Linux distribution, based on Arch Linux built for enhancing college infrastructure and promoting Linux in software engineering education.</b>" \
+    --button=Back \
--- welcome.sh
+++ welcome.sh
@@ -1,16 +1,19 @@
 #!/bin/env bash
 
-yad --title "Welcome To Tcet-Linux" \
+yad --title "Welcome" \
     --form \
+    --window-icon=./assets/tcetlinux-logo.png \
     --columns=2\
     --width=640 \
     --height=380 \
-    --text="This is test tcet-linux Welcome app trial" \
+    --text="Welcome To TCET" \
     --image=./assets/tcetlinux-logo.png \
     --field="<b>Update the PC</b>":fbtn "bash -c './scripts/update.sh'" \
-    --field="<b>Our Github Profile</b>":fbtn "bash -c  './scripts/github.sh'" \
-    --field="<b>Our Discord</b>":fbtn "bash -c './scripts/discord.sh'" \
-    --field="<b>Update Mirror</b>":fbtn "xfce4-terminal -x './scripts/mirror.sh'" \
-    --field="<b>Arch Wiki</b>":fbtn "bash -c './scripts/archwiki.sh'" \
     --field="<b>Screen Resolution</b>":fbtn "xfce4-terminal -x  './scripts/screen-resolution.sh'" \
+    --field="<b>Our Discord</b>":fbtn "bash -c './scripts/discord.sh'" \
+    --field="<b>Arch Wiki</b>":fbtn "bash -c './scripts/archwiki.sh'" \
+    --field="<b>Update Mirror</b>":fbtn "xfce4-terminal -x './scripts/mirror.sh'" \
+    --field="<b>Our Github Profile</b>":fbtn "bash -c  './scripts/github.sh'" \
+    --field="<b>Arch AUR</b>":fbtn "bash -c './scripts/aur.sh'" \
+    --field="<b>About US</b>":fbtn "bash -c  './scripts/about.sh'" \
     --button=Exit:1
