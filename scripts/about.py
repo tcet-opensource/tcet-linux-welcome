4869ff6986cf8496a1da8be4db608e4f6154c659
0xAtharv <atharva.a.vartak@gmail.com>
2023-06-27 23:06:12 +0530

    [FIX] fixed about.py

--- scripts/about.py
+++ scripts/about.py
@@ -4,56 +4,43 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-import os
-from os.path import exists
-from os.path import expanduser
-from pathlib import Path
-
-home = expanduser("~")
-user = os.getlogin()
-
 class aboutWin(Gtk.Window):
     
     def __init__(self):
         super().__init__(title="About Us")
         vbox_left = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        vbox_left.set_homogeneous(False)
+        vbox_left.set_homogeneous(True)
+        self.set_default_geometry(400,100)
         self.set_border_width(10)
-        self.set_default_size(380, 200)
-        self.set_position(Gtk.WindowPosition.CENTER)
+        # self.set_default_size(100, 20)
         self.set_resizable(False)
 
         frame2 = Gtk.Frame(label="TCET-Linux")
 
         grid2 = Gtk.Grid(row_spacing    = 10,
                          column_spacing = 10,
-                         column_homogeneous = True)
+                         column_homogeneous = False)
 
         image1 = Gtk.Image()
         image1.set_from_file("assets/tcetlinux-logo.png")
-        
-
+    
         label1 = Gtk.Label(label="TCET Linux is GNU/Linux distribution, based on Arch Linux built for enhancing college infrastructure and promoting Linux in software engineering education. The primary goal of TCET Open Source is to provide other users with access to project documentation for all of their college projects.")
         label1.set_line_wrap(True)
         label1.set_justify(Gtk.Justification.CENTER)
-        label1.set_max_width_chars(25)
+        label1.set_max_width_chars(50)
+
         label1.set_hexpand(True)
 
         button21 = Gtk.Button(label="Back")
-        button21.set_hexpand(True)
+        # button21.set_hexpand(True)
         button21.connect("clicked", Gtk.main_quit)
         button21.set_property("width-request", 45)
-
+        grid2.attach(button21, 2, 10, 2, 1)
         grid2.attach(image1,   0, 0, 4, 2)
         grid2.attach(label1,   0, 2, 4, 2)
-        grid2.attach(button21, 2, 10, 2, 1)
 
         self.add(frame2)
         frame2.add(grid2)
-
-    def on_button21_clicked(self, widget):
-        print("Exit")
-        button21.connect("clicked", Gtk.main_quit)
 
 win5 = aboutWin()
 
