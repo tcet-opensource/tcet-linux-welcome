f5eb402543e12f0e1a87d69357f32f71bd479925
Rishabh <jharishabh2k3@gmail.com>
2023-06-28 16:53:38 +0530

    [FIX] make the autostart directory if it doesnt exist

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,5 +1,9 @@
 #!/bin/env bash
 
+if [[ ! -d $HOME/.config/autostart ]]; then 
+    mkdir "$HOME"/.config/autostart
+fi
+
 if [[ -f $HOME/.config/autostart/tcet-welcome.desktop ]]; then
     echo "Autostart is enabled";
     echo "Do you want to disable it?"
