c34ba7ce26ceb7f8eb3487db6843e2f99676debd
0xAtharv <atharva.a.vartak@gmail.com>
2023-06-28 12:09:37 +0530

    [FIX] added fixed patch to scripts and .desktop file

--- welcome.sh
+++ welcome.sh
@@ -1,19 +1,22 @@
 #!/bin/env bash
 
+DIR=/usr/share/tcet-linux-welcome
 yad --title "Welcome" \
     --form \
-    --window-icon=./assets/tcetlinux-logo.png \
+    --window-icon=$DIR/assets/tcetlinux-logo.png \
     --columns=2\
+    --rows=5 \
     --width=640 \
     --height=380 \
     --text="<b>Welcome To TCET-Linux</b>" \
-    --image=./assets/tcetlinux-logo.png \
-    --field="<b>Update this PC</b>":fbtn "bash -c './scripts/update.sh'" \
-    --field="<b>Fix Screen Resolution</b>":fbtn "xfce4-terminal -x  './scripts/screen-resolution.sh'" \
-    --field="<b>Our Discord</b>":fbtn "bash -c './scripts/discord.sh'" \
-    --field="<b>Arch Wiki</b>":fbtn "bash -c './scripts/archwiki.sh'" \
-    --field="<b>Update Mirror</b>":fbtn "xfce4-terminal -x './scripts/mirror.sh'" \
-    --field="<b>Our Github Profile</b>":fbtn "bash -c  './scripts/github.sh'" \
-    --field="<b>Arch AUR</b>":fbtn "bash -c './scripts/aur.sh'" \
-    --field="<b>About US</b>":fbtn "bash -c  './scripts/py-about.sh'" \
+    --image=$DIR/assets/tcetlinux-logo.png \
+    --field="<b>Update this PC</b>":fbtn "bash -c '$DIR/scripts/update.sh'" \
+    --field="<b>Fix Screen Resolution</b>":fbtn "xfce4-terminal -x  '$DIR/scripts/screen-resolution.sh'" \
+    --field="<b>Our Discord</b>":fbtn "bash -c '$DIR/scripts/discord.sh'" \
+    --field="<b>Arch Wiki</b>":fbtn "bash -c '$DIR/scripts/archwiki.sh'" \
+    --field="<b>Update Mirror</b>":fbtn "xfce4-terminal -x '$DIR/scripts/mirror.sh'" \
+    --field="<b>Our Github Profile</b>":fbtn "bash -c  '$DIR/scripts/github.sh'" \
+    --field="<b>Arch AUR</b>":fbtn "bash -c '$DIR/scripts/aur.sh'" \
+    --field="<b>About US</b>":fbtn "bash -c  '$DIR/scripts/py-about.sh'" \
     --button=Exit:1
+
