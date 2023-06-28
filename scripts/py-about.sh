eee72afe78d0dc392e29643f8bae585cf18e3a2e
Rishabh <jharishabh2k3@gmail.com>
2023-06-28 17:23:23 +0530

    [FIX] use absolute paths

--- scripts/py-about.sh
+++ scripts/py-about.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-NINSTANCES=$(ps aux | grep 'python ./scripts/about.py'|wc -l )
+NINSTANCES=$(ps aux | grep 'python ~/.local/share/tcet-linux-welcome/scripts/about.py'|wc -l )
 target="1"
 
 if [ "$NINSTANCES" = "$target" ]
 then
-  bash -c "python ./scripts/about.py"
+  bash -c "python ~/.local/share/tcet-linux-welcome/scripts/about.py"
 fi
