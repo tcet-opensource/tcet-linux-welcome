98d8800b68ce0a844fca82f3d93332f7c7f9de7f
Rishabh <jharishabh2k3@gmail.com>
2023-06-28 16:39:39 +0530

    [FIX] fixed the autostart script

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,9 +4,10 @@ if [[ -f $HOME/.config/autostart/tcet-welcome.desktop ]]; then
     echo "Autostart is enabled";
     echo "Do you want to disable it?"
     bool='';
-    read -rpn "Press y/Y if you want to disable autostart" bool;
+    read -rp "Press y/Y if you want to disable autostart - " bool;
     if [[ $bool == y || $bool == Y ]]; then
         rm "$HOME"/.config/autostart/tcet-welcome.desktop;
+        echo "Autostart is disabled now"
     else
         echo "Autostart is still enabled"
     fi
@@ -14,9 +15,10 @@ else
     echo "Autostart is disabled";
     echo "Do you want to enable it?"
     bool='';
-    read -rp '' bool;
+    read -rp "Press y/Y if you want to enable autostart - " bool;
     if [[ $bool == y || $bool == Y ]]; then
         cp ~/.local/share/tcet-linux-welcome/tcet-welcome.desktop "$HOME"/.config/autostart/;
+        echo "Autostart is enabled now"
     else
         echo "Autostart is still disabled"
     fi
