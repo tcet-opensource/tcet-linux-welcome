9d7a5662d0f21b0961478d0132d42a9745e04b8a
harshau007 <amanupadhyay2004@gmail.com>
2023-10-30 20:34:20 +0530

    Removed test.sh file since it does not belog here

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-git clone https://github.com/tcet-opensource/tcet-linux-pkgbuild.git 
-cd tcet-linux-pkgbuild/apps/tcet-linux-welcome
-sed -i '/^pkgrel=/s/pkgrel=.*/pkgrel=$rel/' PKGBUILD
-updatedRel=$rel+1
-echo "rel=$updatedRel" >> $GITHUB_ENV
-cat PKGBUILD
