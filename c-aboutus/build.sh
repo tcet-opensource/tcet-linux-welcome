e5c2d0585af4b744def8a96a1d506051323db678
0xAtharv <atharva.a.vartak@gmail.com>
2023-12-06 14:05:53 +0530

    changed gtk4 -> gtk3 , removed some bugs

--- c-aboutus/build.sh
+++
@@ -1,1 +0,0 @@
- gcc $(pkg-config --cflags gtk4) -o test-aboutus-gtk aboutus.c $(pkg-config --libs gtk4)
---
+++ c-aboutus/g3.sh
@@ -0,0 +1,1 @@
+gcc `pkg-config --cflags gtk+-3.0` aboutus.c -o c_aboutus `pkg-config --libs gtk+-3.0`
