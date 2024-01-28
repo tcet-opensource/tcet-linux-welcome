1ab1a8633cb0c4e83a8f0f2f49d49ede3491ef15
harshau007 <amanupadhyay2004@gmail.com>
2024-01-28 11:52:55 +0530

    CHanged Some changes

--- tcet-welcome/scripts/installer.py
+++ tcet-welcome/scripts/installer.py
@@ -5,6 +5,8 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import subprocess
+import time
+import signal
 
 package_map = {
   "NodeJS": "nodejs",
@@ -26,13 +28,14 @@ class MyApp(Gtk.Window):
     Gtk.Window.__init__(self, title="Application Installer (BETA)")
     self.set_border_width(10)
     self.set_default_size(500, 200)
+    self.set_resizable(False)
 
     grid = Gtk.Grid()
     grid.set_column_spacing(10)
     grid.set_row_spacing(10)
     self.add(grid)
 
-    self.set_icon_from_file(f"{installDir}/assets/tcetlinux-logo.png")
+    # self.set_icon_from_file(f"{installDir}/assets/tcetlinux-logo.png")
 
     self.checkboxes = {}
 
@@ -56,49 +59,30 @@ class MyApp(Gtk.Window):
     grid.attach(button, 2, 2, 1, 1)
     button.connect("clicked", self.on_install_clicked)
 
-  def error_msg(self, package):
-    dialog = Gtk.MessageDialog(transient_for=self,flags=0, message_type=Gtk.MessageType.ERROR,buttons=Gtk.ButtonsType.OK,text="Error installing "+package)
-    dialog.format_secondary_text("Package "+package+" is not available. Please update the mirrorlist and try again.")
-    dialog.run()
-    dialog.destroy()
-    Gtk.main_quit()
-
-  def success_msg(self):
-    dialog = Gtk.MessageDialog(transient_for=self,flags=0, message_type=Gtk.MessageType.ERROR,buttons=Gtk.ButtonsType.OK,text="Packages Installed ")
-    dialog.format_secondary_text("Packages are installed successfully.")
-    dialog.run()
-    dialog.destroy()
-    Gtk.main_quit()
-
   def clear_checkboxes(self):
     for checkbox in self.checkboxes.values():
       checkbox.set_active(False)
 
   def on_install_clicked(self, widget):
     to_install = [v for k,v in package_map.items() if self.checkboxes[k].get_active()]
-    # error = False
     installCMD = "yay -S --noconfirm"
     for package in to_install:
       installCMD = installCMD + " " + package
-        # subprocess.run(["pkexec", "yay", "-S", package, "--noconfirm"])
-        # try:
-        #   subprocess.check_output(["pacman", "-Q", package]).decode().split("\n")
-        # except subprocess.CalledProcessError:
-        #   self.error_msg(package)
-        #   error = True
     result = os.popen('echo $XDG_CURRENT_DESKTOP').read().strip()
     match result:
       case "GNOME":
-        subprocess.run(["kgx", "-e", installCMD])
+        p = subprocess.Popen(["kgx", "--", "bash", "-c", installCMD], preexec_fn=os.setpgrp)
+        time.sleep(25)
+        os.killpg(p.pid, signal.SIGINT)
       case "XFCE":
-        subprocess.run(["xfce4-terminal", "-e", installCMD])
+        p = subprocess.Popen(["xfce4-terminal", "--", "bash", "-c", installCMD], preexec_fn=os.setpgrp)
+        time.sleep(25)
+        os.killpg(p.pid, signal.SIGINT)
       case _:
         # yad --image="dialog-question" --title "Alert" --text "Can't recongnize desktop environment" --button="yad-ok:0"
         print("Cant Recognize DE")
 
     self.clear_checkboxes()
-    # if not error:
-    #   self.success_msg()
 
 win = MyApp()
 win.connect("destroy", Gtk.main_quit)
